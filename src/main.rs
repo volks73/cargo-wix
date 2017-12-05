42fc9ce491a43cd43cb89a25141ce743b89001d5
Christopher Field <cfield2@gmail.com>
2017-12-05 15:19:13 -0500

    Add `--sign` flag
    If used, then the signtool will be used to sign the installer after it
    is built.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
-extern crate clap;
+#[macro_use] extern crate clap;
 extern crate toml;
 
+use clap::{App, Arg, SubCommand};
 use std::fs::File;
 use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use toml::Value;
 
+const SUBCOMMAND_NAME: &str = "wix";
 const WIX_TOOLSET_COMPILER: &str = "candle";
 const WIX_TOOLSET_LINKER: &str = "light";
 const SIGNTOOL: &str = "signtool";
 
 fn main() {
+    // TODO: Add ansi color support
+    // TODO: Add verbosity logging
+    let matches = App::new(crate_name!())
+        .bin_name("cargo")
+        .subcommand(
+            SubCommand::with_name(SUBCOMMAND_NAME)
+                .version(crate_version!())
+                .about(crate_description!())
+                .author(crate_authors!())
+                .arg(Arg::with_name("sign")
+                     .help("The Windows installer (msi) will be signed using the SignTool application available in the Windows 10 SDK. The signtool is invoked with the '/a' flag to automatically obtain an appropriate certificate from the Windows certificate manager. The default is to also use the Comodo timestamp server with the '/t' flag.")
+                     .short("s")
+                     .long("sign"))
+        ).get_matches();
+    let matches = matches.subcommand_matches(SUBCOMMAND_NAME).unwrap();
     let cargo_file_path = Path::new("Cargo.toml");
     let mut cargo_file = File::open(cargo_file_path).expect("Open Cargo.toml file");
     let mut cargo_file_content = String::new();
@@ -29,24 +46,33 @@ fn main() {
         .and_then(|t| t.get("name"))
         .and_then(|n| n.as_str())
         .expect("Package name");
-    let mut installer_wxs = PathBuf::from("wix");
-    installer_wxs.push("main");
-    installer_wxs.set_extension("wxs");
-    let mut installer_wixobj = PathBuf::from("target");
-    installer_wixobj.push("wix");
-    installer_wixobj.push("build");
-    installer_wixobj.push("installer");
-    installer_wixobj.set_extension("wixobj");
-    let mut installer_msi = PathBuf::from("target");
-    installer_msi.push("wix");
-    installer_msi.push("installer");
-    installer_msi.push(&format!("{}-{}-win64", pkg_name, pkg_version));
-    installer_msi.set_extension("msi");
+    let mut main_wxs = PathBuf::from("wix");
+    main_wxs.push("main");
+    main_wxs.set_extension("wxs");
+    let mut main_wixobj = PathBuf::from("target");
+    main_wixobj.push("wix");
+    main_wixobj.push("build");
+    main_wixobj.push("main");
+    main_wixobj.set_extension("wixobj");
+    let mut main_msi = PathBuf::from("target");
+    main_msi.push("wix");
+    main_msi.push(&format!("{}-{}-win64", pkg_name, pkg_version));
+    main_msi.set_extension("msi");
+    // Build a release executable
+    if let Some(status) = Command::new("cargo")
+        .arg("build")
+        .arg("--release")
+        .status()
+        .ok() {
+        if !status.success() {
+            panic!("Failed to build the release executable");
+        }
+    }
     // Compile the installer
     if let Some(status) = Command::new(WIX_TOOLSET_COMPILER)
         .arg("-o")
-        .arg(&installer_wixobj)
-        .arg(&installer_wxs)
+        .arg(&main_wixobj)
+        .arg(&main_wxs)
         .status()
         .ok() {
         if !status.success() {
@@ -58,9 +84,9 @@ fn main() {
         .arg("-ext")
         .arg("WixUIExtension")
         .arg("-cultures:en-us")
-        .arg(&installer_wixobj)
+        .arg(&main_wixobj)
         .arg("-out")
-        .arg(&installer_msi)
+        .arg(&main_msi)
         .status()
         .ok() {
         if !status.success() {
@@ -68,16 +94,19 @@ fn main() {
         }
     }
     // Sign the installer
-    if let Some(status) = Command::new(SIGNTOOL)
-        .arg("sign")
-        .arg("/a")
-        .arg("/t")
-        .arg("http://timestamp.comodoca.com")
-        .arg(&installer_msi)
-        .status()
-        .ok() {
-        if !status.success() {
-            panic!("Failed to sign the installer");
+    if matches.is_present("sign") {
+        if let Some(status) = Command::new(SIGNTOOL)
+            .arg("sign")
+            .arg("/a")
+            .arg("/t")
+            .arg("http://timestamp.comodoca.com")
+            .arg(&main_msi)
+            .status()
+            .ok() {
+            if !status.success() {
+                panic!("Failed to sign the installer");
+            }
         }
     }
+    // TODO: Add error handling
 }
