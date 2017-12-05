d45ab52b2da3ab5f846b8d72d3e12515e7dd5890
Christopher Field <cfield2@gmail.com>
2017-12-05 16:23:44 -0500

    Add xml preprocessor variables
    The crate/package values are used for the values of xml preprocessor
    variables in the main.wxs. These variables include: Version, Platform,
    ProductName, BinaryName, and Description. This way the Cargo.toml file
    because the sole source of truth and generating the installer with
    package specific information becomes automatic.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use] extern crate clap;
 extern crate toml;
+extern crate loggerv;
 
 use clap::{App, Arg, SubCommand};
 use std::fs::File;
@@ -27,25 +28,45 @@ fn main() {
                      .help("The Windows installer (msi) will be signed using the SignTool application available in the Windows 10 SDK. The signtool is invoked with the '/a' flag to automatically obtain an appropriate certificate from the Windows certificate manager. The default is to also use the Comodo timestamp server with the '/t' flag.")
                      .short("s")
                      .long("sign"))
+                .arg(Arg::with_name("win64")
+                     .help("Builds the installer for the x64 platform. The default is to build the installer for the x86 platform.")
+                     .long("win64"))
         ).get_matches();
     let matches = matches.subcommand_matches(SUBCOMMAND_NAME).unwrap();
+    let platform = if matches.is_present("win64") {
+        "x64"
+    } else {
+        "x86"
+    };
     let cargo_file_path = Path::new("Cargo.toml");
     let mut cargo_file = File::open(cargo_file_path).expect("Open Cargo.toml file");
     let mut cargo_file_content = String::new();
     cargo_file.read_to_string(&mut cargo_file_content).expect("Read to string");
-    let pkg_values = cargo_file_content.parse::<Value>().expect("Parse cargo file contents");
-    let pkg_version = pkg_values
+    let cargo_values = cargo_file_content.parse::<Value>().expect("Parse cargo file contents");
+    let pkg_version = cargo_values
         .get("package")
         .and_then(|p| p.as_table())
         .and_then(|t| t.get("version"))
         .and_then(|v| v.as_str())
         .expect("Package version");
-    let pkg_name = pkg_values
+    let pkg_name = cargo_values
         .get("package")
         .and_then(|p| p.as_table())
         .and_then(|t| t.get("name"))
         .and_then(|n| n.as_str())
         .expect("Package name");
+    let pkg_description = cargo_values
+        .get("package")
+        .and_then(|p| p.as_table())
+        .and_then(|t| t.get("description"))
+        .and_then(|d| d.as_str())
+        .expect("Package description");
+    let bin_name = cargo_values
+        .get("bin")
+        .and_then(|b| b.as_table())
+        .and_then(|t| t.get("name"))
+        .and_then(|n| n.as_str())
+        .unwrap_or(pkg_name);
     let mut main_wxs = PathBuf::from("wix");
     main_wxs.push("main");
     main_wxs.set_extension("wxs");
@@ -56,9 +77,13 @@ fn main() {
     main_wixobj.set_extension("wixobj");
     let mut main_msi = PathBuf::from("target");
     main_msi.push("wix");
-    main_msi.push(&format!("{}-{}-win64", pkg_name, pkg_version));
-    main_msi.set_extension("msi");
-    // Build a release executable
+    // Do NOT use the `set_extension` method for the MSI path. Since the pkg_version is in X.X.X
+    // format, the `set_extension` method will replace the Patch version number and
+    // architecture/platform with `msi`.  Instead, just include the extension in the formatted
+    // name.
+    main_msi.push(&format!("{}-{}-{}.msi", pkg_name, pkg_version, platform));
+    // Build the binary with the release profile. A release binary has already been built, this
+    // will essentially do nothing.
     if let Some(status) = Command::new("cargo")
         .arg("build")
         .arg("--release")
@@ -70,6 +95,11 @@ fn main() {
     }
     // Compile the installer
     if let Some(status) = Command::new(WIX_TOOLSET_COMPILER)
+        .arg(format!("-dVersion={}", pkg_version))
+        .arg(format!("-dPlatform={}", platform))
+        .arg(format!("-dProductName={}", pkg_name))
+        .arg(format!("-dBinaryName={}", bin_name))
+        .arg(format!("-dDescription={}", pkg_description))
         .arg("-o")
         .arg(&main_wixobj)
         .arg(&main_wxs)
