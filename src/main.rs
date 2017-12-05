9ff83cc8c58a6dc54ba659bf7da80fdbe8e7af98
Christopher Field <cfield2@gmail.com>
2017-12-05 16:38:57 -0500

    Add verbosity logging and ansi color support

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-#[macro_use] extern crate clap;
-extern crate toml;
-extern crate loggerv;
+// Copyright (C) 2017 Christopher R. Field.
 
+extern crate ansi_term;
+extern crate atty;
+#[macro_use] extern crate clap;
+extern crate log;
+extern crate loggerv;
+extern crate toml;
+
+use ansi_term::Colour;
 use clap::{App, Arg, SubCommand};
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use toml::Value;
@@ -14,8 +20,14 @@ const WIX_TOOLSET_COMPILER: &str = "candle";
 const WIX_TOOLSET_LINKER: &str = "light";
 const SIGNTOOL: &str = "signtool";
 
+const ERROR_COLOR: Colour = Colour::Fixed(9); // Bright red
+
 fn main() {
-    // TODO: Add ansi color support
+    // Based on documentation for the ansi_term crate, Windows 10 supports ANSI escape characters,
+    // but it must be enabled first. The ansi_term crate provides a function for enabling ANSI
+    // support in Windows, but it is conditionally compiled and only exists for Windows builds. To
+    // avoid build errors on non-windows platforms, a cfg guard should be put in place.
+    #[cfg(windows)] ansi_term::enable_ansi_support().expect("Enable ANSI support on Windows");
     // TODO: Add verbosity logging
     let matches = App::new(crate_name!())
         .bin_name("cargo")
@@ -31,8 +43,24 @@ fn main() {
                 .arg(Arg::with_name("win64")
                      .help("Builds the installer for the x64 platform. The default is to build the installer for the x86 platform.")
                      .long("win64"))
+                .arg(Arg::with_name("verbose")
+                     .help("Sets the level of verbosity. The higher the level of verbosity, the more information that is printed and logged when the application is executed. This flag can be specified multiple times, where each occurrance increases the level and/or details written for each statement.")
+                     .long("verbose")
+                     .short("v")
+                     .multiple(true))
         ).get_matches();
     let matches = matches.subcommand_matches(SUBCOMMAND_NAME).unwrap();
+    if matches.occurrences_of("verbose") > 3 {
+        loggerv::Logger::new()
+            .line_numbers(true)
+            .module_path(true)
+    } else {
+        loggerv::Logger::new()
+            .module_path(false)
+    }.verbosity(matches.occurrences_of("verbose"))
+    .level(true)
+    .init()
+    .expect("logger to initiate");
     let platform = if matches.is_present("win64") {
         "x64"
     } else {
@@ -138,5 +166,19 @@ fn main() {
             }
         }
     }
-    // TODO: Add error handling
+    // TODO: Wrap execution into a function that returns a Result
+    //match result {
+        //Ok(_) => {
+            //std::process::exit(0);
+        //},
+        //Err(e) => {
+            //let mut tag = format!("Error[{}] ({})", e.code(), e.description());
+            //if atty::is(atty::Stream::Stderr) {
+                //tag = ERROR_COLOR.paint(tag).to_string()
+            //}
+            //writeln!(&mut std::io::stderr(), "{}: {}", tag, e)
+                //.expect("Writing to stderr");
+            //std::process::exit(e.code());
+        //}
+    //}
 }
