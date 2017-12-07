4a55738d2bdc2f3c57a049f17d732d670b5c2443
Christopher Field <cfield2@gmail.com>
2017-12-07 09:35:41 -0500

    Add rustdoc files

---
+++ target/doc/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cargo_wix"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"cargo_wix/enum.Error.html\" title=\"enum cargo_wix::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"cargo_wix/enum.Platform.html\" title=\"enum cargo_wix::Platform\">Platform</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"cargo_wix/struct.Wix.html\" title=\"struct cargo_wix::Wix\">Wix</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
