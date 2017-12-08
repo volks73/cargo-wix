56811ac317ebe7c6027b87befbd0060ee875ec47
Christopher Field <cfield2@gmail.com>
2017-12-08 09:35:25 -0500

    Add new API docs

---
+++ implementors/core/str/trait.FromStr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cargo_wix"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"enum\" href=\"cargo_wix/enum.TimestampServer.html\" title=\"enum cargo_wix::TimestampServer\">TimestampServer</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
