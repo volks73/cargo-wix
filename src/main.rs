c6037ebb5c52cdf20cd798cfd422be795b6f549b
Christopher Field <cfield2@gmail.com>
2018-05-10 09:23:09 -0400

    Refactor long static string formatting
    Line breaks are added for long static strings to make the source code
    more readable.

--- src/main.rs
+++ src/main.rs
@@ -37,12 +37,15 @@ fn main() {
                 .version(crate_version!())
                 .about(crate_description!())
                 .arg(Arg::with_name("bin-path")
-                    .help("Specifies the path to the WiX Toolset's 'bin' folder, which should contain the needed 'candle.exe' and 'light.exe' applications. The default is to use the PATH system environment variable.")
+                    .help("Specifies the path to the WiX Toolset's 'bin' folder, which should \
+                          contain the needed 'candle.exe' and 'light.exe' applications. The default \
+                          is to use the PATH system environment variable.")
                     .long("bin-path")
                     .short("B")
                     .takes_value(true))
                 .arg(Arg::with_name("binary-name")
-                    .help("Overrides the 'name' field of the bin section of the package's manifest (Cargo.toml) as the name of the executable within the installer.")
+                    .help("Overrides the 'name' field of the bin section of the package's manifest \
+                          (Cargo.toml) as the name of the executable within the installer.")
                     .long("binary-name")
                     .short("b")
                     .takes_value(true))
@@ -53,49 +56,72 @@ fn main() {
                     .conflicts_with("sign")
                     .conflicts_with("print-template"))
                 .arg(Arg::with_name("description")
-                    .help("Overrides the 'description' field of the package's manifest (Cargo.toml) as the description within the installer.")
+                    .help("Overrides the 'description' field of the package's manifest (Cargo.toml) \
+                          as the description within the installer.")
                     .long("description")
                     .short("d")
                     .takes_value(true))
                 .arg(Arg::with_name("force")
-                    .help("Overwrites any existing WiX Source files when using the '--init' flag. Use with caution.")
+                    .help("Overwrites any existing WiX Source files when using the '--init' flag. \
+                          Use with caution.")
                     .long("force")
                     .requires("init"))
                 .arg(Arg::with_name("holder")
-                    .help("Sets the copyright holder for the license during initialization. The default is to use the first author from the package's manifest (Cargo.toml). This requires the '--init' flag.")
+                    .help("Sets the copyright holder for the license during initialization. The \
+                          default is to use the first author from the package's manifest \
+                          (Cargo.toml). This requires the '--init' flag.")
                     .long("holder")
                     .short("H")
                     .takes_value(true)
                     .requires("init"))
                 .arg(Arg::with_name("init")
-                    .help("Initializes the package to be used with this subcommand. This creates a 'wix` sub-folder within the root folder of the package and creates a 'main.wxs' WiX Source (wxs) file within the 'wix' sub-folder from the embedded template. The 'wix\\main.wxs' file that is created can immediately be used with this subcommand without modification to create an installer for the project.")
+                    .help("Initializes the package to be used with this subcommand. This creates \
+                          a 'wix` sub-folder within the root folder of the package and creates \
+                          a 'main.wxs' WiX Source (wxs) file within the 'wix' sub-folder from the \
+                          embedded template. The 'wix\\main.wxs' file that is created can \
+                          immediately be used with this subcommand without modification to create \
+                          an installer for the project.")
                     .long("init")
                     .conflicts_with("clean")
                     .conflicts_with("purge")
                     .conflicts_with("print-template"))
                 .arg(Arg::with_name("license")
-                    .help("Overrides the 'license-file' field of the package's manfiest (Cargo.toml) as the file to be converted to the 'License.txt' file that is added to the install location along side the 'bin' folder by the installer.")
+                    .help("Overrides the 'license-file' field of the package's manfiest \
+                          (Cargo.toml) as the file to be converted to the 'License.txt' file that \
+                          is added to the install location along side the 'bin' folder by the \
+                          installer.")
                     .long("license")
                     .short("l")
                     .takes_value(true))
                 .arg(Arg::with_name("manufacturer")
-                    .help("Overrides the first author in the 'authors' field of the package's manifest (Cargo.toml) as the manufacturer within the installer.")
+                    .help("Overrides the first author in the 'authors' field of the package's \
+                          manifest (Cargo.toml) as the manufacturer within the installer.")
                     .long("manufacturer")
                     .short("m")
                     .takes_value(true))
                 .arg(Arg::with_name("no-build")
-                    .help("Skips building the release binary. The installer is created, but the 'cargo build --release' is not executed.")
+                    .help("Skips building the release binary. The installer is created, but the \
+                          'cargo build --release' is not executed.")
                     .long("no-build"))
                 .arg(Arg::with_name("no-capture")
-                    .help("By default, this subcommand captures, or hides, all output from the builder, compiler, linker, and signer for the binary and Windows installer, respectively. Use this flag to show the output.")
+                    .help("By default, this subcommand captures, or hides, all output from the \
+                          builder, compiler, linker, and signer for the binary and Windows \
+                          installer, respectively. Use this flag to show the output.")
                     .long("nocapture"))
                 .arg(Arg::with_name("output")
-                    .help("Sets the destination file name and path for the created installer. The default is to create an installer with the '<product-name>-<version>-<arch>.msi' file name in the 'target\\wix' folder.")
+                    .help("Sets the destination file name and path for the created installer. The \
+                          default is to create an installer with the \
+                          '<product-name>-<version>-<arch>.msi' file name in the 'target\\wix' \
+                          folder.")
                     .long("output")
                     .short("o")
                     .takes_value(true))
                 .arg(Arg::with_name("print-template")
-                    .help("Prints a template to stdout. In the case of a license template, the output is in the Rich Text Format (RTF) and for a WiX Source file, the output is in XML. New GUIDS are generated for the 'UpgradeCode' and Path Component each time the 'WXS' template is printed. Values are case insensitive. [values: Apache-2.0, GPL-3.0, MIT, WXS]")
+                    .help("Prints a template to stdout. In the case of a license template, the \
+                          output is in the Rich Text Format (RTF) and for a WiX Source file, the \
+                          output is in XML. New GUIDS are generated for the 'UpgradeCode' and Path \
+                          Component each time the 'WXS' template is printed. Values are case \
+                          insensitive. [values: Apache-2.0, GPL-3.0, MIT, WXS]")
                     .long("print-template")
                     .hide_possible_values(true)
                     .possible_values(&Template::possible_values()
@@ -107,7 +133,8 @@ fn main() {
                     .conflicts_with("clean")
                     .conflicts_with("purge"))
                 .arg(Arg::with_name("product-name")
-                    .help("Overrides the 'name' field of the package's manifest (Cargo.toml) as the product name within the installer.")
+                    .help("Overrides the 'name' field of the package's manifest (Cargo.toml) as the \
+                          product name within the installer.")
                     .long("product-name")
                     .short("p")
                     .takes_value(true))
@@ -119,29 +146,42 @@ fn main() {
                     .conflicts_with("clean")
                     .conflicts_with("print-template"))
                 .arg(Arg::with_name("sign")
-                    .help("The Windows installer (msi) will be signed using the SignTool application available in the Windows 10 SDK. The signtool is invoked with the '/a' flag to automatically obtain an appropriate certificate from the Windows certificate manager. The default is to also use the Comodo timestamp server with the '/t' flag.")
+                    .help("The Windows installer (msi) will be signed using the SignTool \
+                          application available in the Windows 10 SDK. The signtool is invoked with \
+                          the '/a' flag to automatically obtain an appropriate certificate from the \
+                          Windows certificate manager. The default is to also use the Comodo \
+                          timestamp server with the '/t' flag.")
                     .short("s")
                     .long("sign"))
                 .arg(Arg::with_name("sign-path")
-                    .help("Specifies the path to the folder containg the 'signtool' application. The default is to use the PATH system environment variable to locate the application. This can only be used with the '-s,--sign' flag.")
+                    .help("Specifies the path to the folder containg the 'signtool' application. \
+                          The default is to use the PATH system environment variable to locate the \
+                          application. This can only be used with the '-s,--sign' flag.")
                     .long("sign-path")
                     .short("S")
                     .takes_value(true)
                     .requires("sign"))
                 .arg(Arg::with_name("timestamp")
-                    .help("The alias or URL for the timestamp server used with the 'signtool' to sign the installer. This can only be used with the '-s,--sign' flag. Either an alias can be used or a URL. Available case-insensitive aliases include: Comodo and Verisign.")
+                    .help("The alias or URL for the timestamp server used with the 'signtool' to \
+                          sign the installer. This can only be used with the '-s,--sign' flag. \
+                          Either an alias can be used or a URL. Available case-insensitive aliases \
+                          include: Comodo and Verisign.")
                     .short("t")
                     .long("timestamp")
                     .takes_value(true)
                     .requires("sign"))
                 .arg(Arg::with_name("year")
-                     .help("Sets the copyright year for the license during initialization. The default is to use the current year. This requires the '--init' flag.")
+                     .help("Sets the copyright year for the license during initialization. The \
+                           default is to use the current year. This requires the '--init' flag.")
                      .short("Y")
                      .long("year")
                      .takes_value(true)
                      .requires("init"))
                 .arg(Arg::with_name("verbose")
-                    .help("Sets the level of verbosity. The higher the level of verbosity, the more information that is printed and logged when the application is executed. This flag can be specified multiple times, where each occurrance increases the level and/or details written for each statement.")
+                    .help("Sets the level of verbosity. The higher the level of verbosity, the more \
+                          information that is printed and logged when the application is executed. \
+                          This flag can be specified multiple times, where each occurrance \
+                          increases the level and/or details written for each statement.")
                     .long("verbose")
                     .short("v")
                     .multiple(true))
