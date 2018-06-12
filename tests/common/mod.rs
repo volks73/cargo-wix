ecfdb8232d495856814e99608c912c05e1598255
Christopher Field <cfield2@gmail.com>
2018-06-12 16:39:22 -0400

    Fix error with test package names and XML

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -4,7 +4,10 @@ use std::process::Command;
 
 pub fn create_test_package() -> tempfile::TempDir {
     // Use a prefix because the default `.tmp` is an invalid name for a Cargo package.
-    let temp_dir = tempfile::Builder::new().prefix("cargo-wix-test-").tempdir().unwrap();
+    // 
+    // Cannot use dashes. WiX Toolset only allows A-Z, a-z, digits, underscores (_), or periods (.)
+    // for attribute IDs.
+    let temp_dir = tempfile::Builder::new().prefix("cargo_wix_test_").tempdir().unwrap();
     let cargo_init_status = Command::new("cargo")
         .arg("init")
         .arg("--bin")
