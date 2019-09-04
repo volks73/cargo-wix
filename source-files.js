cb10822e57aaa14fbff361c0ac2335a1754e5c96
cfield <chris@fieldrndservices.com>
2019-09-04 13:43:07 -0400

    Add new files from cargo doc

---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["cargo_wix"] = {"name":"","files":["main.rs"]};
+sourcesIndex["wix"] = {"name":"","dirs":[{"name":"print","files":["license.rs","mod.rs","wxs.rs"]},{"name":"templates","files":["mod.rs"]}],"files":["clean.rs","create.rs","eula.rs","initialize.rs","lib.rs","purge.rs","sign.rs"]};
+createSourceSidebar();
