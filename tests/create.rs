9d79d34f7ccad31560b00c1982492383ddb9ca16
cfield <chris@fieldrndservices.com>
2020-02-10 10:00:34 -0500

    Add integration tests for compiler and linker arguments

--- tests/create.rs
+++ tests/create.rs
@@ -817,3 +817,60 @@ fn includes_works_with_input_outside_cwd() {
         .child(expected_msi_file.path())
         .assert(predicate::path::exists());
 }
+
+#[test]
+fn compiler_args_flags_only_works() {
+    init_logging();
+    let original_working_directory = env::current_dir().unwrap();
+    let package = common::create_test_package();
+    let expected_msi_file = TARGET_WIX_DIR.join(format!("{}-0.1.0-x86_64.msi", PACKAGE_NAME));
+    env::set_current_dir(package.path()).unwrap();
+    initialize::Builder::default().build().run().unwrap();
+    let result = run(Builder::default().compiler_args(Some(vec!["-nologo", "-wx"])));
+    env::set_current_dir(original_working_directory).unwrap();
+    result.expect("OK result");
+    package
+        .child(TARGET_WIX_DIR.as_path())
+        .assert(predicate::path::exists());
+    package
+        .child(expected_msi_file)
+        .assert(predicate::path::exists());
+}
+
+#[test]
+fn compiler_args_options_works() {
+    init_logging();
+    let original_working_directory = env::current_dir().unwrap();
+    let package = common::create_test_package();
+    let expected_msi_file = TARGET_WIX_DIR.join(format!("{}-0.1.0-x86_64.msi", PACKAGE_NAME));
+    env::set_current_dir(package.path()).unwrap();
+    initialize::Builder::default().build().run().unwrap();
+    let result = run(Builder::default().compiler_args(Some(vec!["-arch", "x64"])));
+    env::set_current_dir(original_working_directory).unwrap();
+    result.expect("OK result");
+    package
+        .child(TARGET_WIX_DIR.as_path())
+        .assert(predicate::path::exists());
+    package
+        .child(expected_msi_file)
+        .assert(predicate::path::exists());
+}
+
+#[test]
+fn linker_args_flags_only_works() {
+    init_logging();
+    let original_working_directory = env::current_dir().unwrap();
+    let package = common::create_test_package();
+    let expected_msi_file = TARGET_WIX_DIR.join(format!("{}-0.1.0-x86_64.msi", PACKAGE_NAME));
+    env::set_current_dir(package.path()).unwrap();
+    initialize::Builder::default().build().run().unwrap();
+    let result = run(Builder::default().compiler_args(Some(vec!["-nologo", "-wx"])));
+    env::set_current_dir(original_working_directory).unwrap();
+    result.expect("OK result");
+    package
+        .child(TARGET_WIX_DIR.as_path())
+        .assert(predicate::path::exists());
+    package
+        .child(expected_msi_file)
+        .assert(predicate::path::exists());
+}
