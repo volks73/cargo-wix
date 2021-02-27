dbd45b05b3f30600e195c53b9877e3a34c90470c
Chris Field <chris@fieldrndservices.com>
2021-02-27 18:59:43 -0500

    Change copying implementation
    The `fs_extra` was just too confusing and not sure if it was actually
    working or what any of the options meant.

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,14 +1,61 @@
 use anyhow::{bail, Result};
 use structopt::StructOpt;
-use fs_extra::dir::CopyOptions;
 
+use std::fs;
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 
 #[derive(Debug, StructOpt)]
 enum XTask {
     Ghp,
+}
+
+fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<()> {
+    let mut stack = Vec::new();
+    stack.push(PathBuf::from(from.as_ref()));
+
+    let output_root = PathBuf::from(to.as_ref());
+    let input_root = PathBuf::from(from.as_ref()).components().count();
+
+    while let Some(working_path) = stack.pop() {
+        println!("process: {:?}", &working_path);
+
+        // Generate a relative path
+        let src: PathBuf = working_path.components().skip(input_root).collect();
+
+        // Create a destination if missing
+        let dest = if src.components().count() == 0 {
+            output_root.clone()
+        } else {
+            output_root.join(&src)
+        };
+        if fs::metadata(&dest).is_err() {
+            println!("mkdir: {:?}", dest);
+            fs::create_dir_all(&dest)?;
+        }
+
+        for entry in fs::read_dir(working_path)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                stack.push(path);
+            } else {
+                match path.file_name() {
+                    Some(filename) => {
+                        let dest_path = dest.join(filename);
+                        println!("copy: {:?} -> {:?}", &path, &dest_path);
+                        fs::copy(&path, &dest_path)?;
+                    }
+                    None => {
+                        println!("failed: {:?}", path);
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
 
 fn main() -> Result<()> {
@@ -29,24 +76,9 @@ fn main() -> Result<()> {
             }
             let mut target_doc_dir = PathBuf::from("target");
             target_doc_dir.push("doc");
-            fs_extra::copy_items_with_progress(
-                &target_doc_dir
-                    .read_dir()?
-                    .map(|entry| entry.unwrap().path())
-                    .collect::<Vec<PathBuf>>(),
-                &env::current_dir()?,
-                &CopyOptions::new(),
-                // &CopyOptions {
-                //     overwrite: true,
-                //     skip_exist: false,
-                //     copy_inside: true,
-                //     ..CopyOptions::new()
-                // },
-                |info| {
-                    println!("{} in {}: {}", info.file_name, info.dir_name, info.total_bytes);
-                    fs_extra::dir::TransitProcessResult::ContinueOrAbort
-                }
-            )?;
+            copy(target_doc_dir, env::current_dir()?)?;
+            // TODO: Add committing changes
+            // TODO: Add pushing changes to repository
             let status = Command::new("git").arg("checkout").arg("main").status()?;
             if !status.success() {
                 bail!("The 'git checkout main' command failed");
