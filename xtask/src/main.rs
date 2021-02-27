f130bfff4a68aa2dc0ed04ec76b0382200c91d4d
Chris Field <chris@fieldrndservices.com>
2021-02-27 17:53:15 -0500

    Add running rustdoc from xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,14 +1,31 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use structopt::StructOpt;
+
+use std::env;
+use std::process::Command;
+use std::path::PathBuf;
 
 #[derive(Debug, StructOpt)]
 enum XTask {
-    #[structopt(alias = "ghp")]
     Ghp,
 }
 
 fn main() -> Result<()> {
     let args = XTask::from_args();
-    println!("{:?}", args);
+    match args {
+        XTask::Ghp => {
+            let cargo = env::var("CARGO")
+                .map(PathBuf::from)
+                .ok()
+                .unwrap_or_else(|| PathBuf::from("cargo"));
+            let status = Command::new(cargo)
+                .arg("doc")
+                .arg("--no-deps")
+                .status()?;
+            if !status.success() {
+                bail!("The 'cargo doc' command failed");
+            }
+        }
+    }
     Ok(())
 }
